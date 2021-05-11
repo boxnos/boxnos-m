8536ecbca5a6c5a6c70be531514b5d993719ef20
boxnos <boxnos@yahoo.com>
2021-05-11 22:50:34 +0900

    Hello, world * 3

--- loader/main.c
+++ loader/main.c
@@ -1,10 +1,10 @@
 #include  <Uefi.h>
 #include  <Library/UefiLib.h>
 
-EFI_STATUS EFIAPI UefiMain(
-    EFI_HANDLE image_handle,
-    EFI_SYSTEM_TABLE *system_table) {
-  Print(L"Hello, boxnos loader World!\n");
-  while (1);
-  return EFI_SUCCESS;
+EFI_STATUS EFIAPI uefi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table) {
+    for (int i = 0; i < 3; i++)
+        Print(L"Hello, boxnos loader World!\n");
+    for (;;)
+        ;
+    return EFI_SUCCESS;
 }
