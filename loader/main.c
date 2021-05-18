f6ba2fdbfdb76c8356b7e6d7dece9cc79601cdef
boxnos <boxnos@yahoo.com>
2021-05-19 04:13:42 +0900

    Print test

--- loader/main.c
+++ loader/main.c
@@ -3,7 +3,8 @@
 
 EFI_STATUS EFIAPI uefi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table) {
     for (int i = 0; i < 3; i++)
-        Print(L"Hello, boxnos loader World!\n");
+        Print(L"BOOTING BOXNOS-M ... %d\n", i);
+    Print(L"DONE.");
     for (;;)
         ;
     return EFI_SUCCESS;
