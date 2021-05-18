fca1d6429952ffeaca93d9a31a055ac19528684a
boxnos <boxnos@yahoo.com>
2021-05-19 04:29:39 +0900

    Fix typo

--- loader/main.c
+++ loader/main.c
@@ -5,7 +5,7 @@
 EFI_STATUS EFIAPI uefi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table) {
     for (int i = 0; i < 3; i++)
         Print(L"BOOTING BOXNOS-M ... %d\n", i);
-x
+
     CHAR8 buf[4096 * 4];
     UINTN buf_size = sizeof(buf), map_key, discriptor_size;
     UINT32 discriptor_version;
