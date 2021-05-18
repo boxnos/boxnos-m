3787cb87b17d61f6448aec1cad8f7581978fe6e2
boxnos <boxnos@yahoo.com>
2021-05-19 04:26:01 +0900

    test GetMemoryMap

--- loader/main.c
+++ loader/main.c
@@ -1,9 +1,21 @@
 #include  <Uefi.h>
 #include  <Library/UefiLib.h>
+#include  <Library/UefiBootServicesTableLib.h>
 
 EFI_STATUS EFIAPI uefi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table) {
     for (int i = 0; i < 3; i++)
         Print(L"BOOTING BOXNOS-M ... %d\n", i);
+x
+    CHAR8 buf[4096 * 4];
+    UINTN buf_size = sizeof(buf), map_key, discriptor_size;
+    UINT32 discriptor_version;
+
+    Print(L"buf_size %d\n", buf_size);
+
+    gBS->GetMemoryMap(&buf_size, (EFI_MEMORY_DESCRIPTOR*) buf, &map_key, &discriptor_size, &discriptor_version);
+
+    Print(L"buf_size %d\n", buf_size);
+
     Print(L"DONE.");
     for (;;)
         ;
