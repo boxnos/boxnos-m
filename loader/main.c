69481a6b80dd10a50b8df063e9ed1ec311f07270
boxnos <boxnos@yahoo.com>
2021-05-20 20:59:36 +0900

    Print MemoryMap roughly

--- loader/main.c
+++ loader/main.c
@@ -6,16 +6,21 @@ EFI_STATUS EFIAPI uefi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_ta
     for (int i = 0; i < 3; i++)
         Print(L"BOOTING BOXNOS-M ... %d\n", i);
 
-    EFI_MEMORY_DESCRIPTOR buf[400];
+    CHAR8 buf[1024 * 4 * 4];
     UINTN buf_size = sizeof(buf), map_key, discriptor_size;
     UINT32 discriptor_version;
 
+    gBS->GetMemoryMap(&buf_size, (EFI_MEMORY_DESCRIPTOR *) buf, &map_key, &discriptor_size, &discriptor_version);
+    Print(L"buf_size %d\n", buf_size);
     Print(L"Discriptor size %d\n", sizeof(EFI_MEMORY_DESCRIPTOR));
-    Print(L"buf_size %d\n", buf_size);
+    Print(L"discriptor_size %d\n", discriptor_size);
 
-    gBS->GetMemoryMap(&buf_size, buf, &map_key, &discriptor_size, &discriptor_version);
+    buf_size /= discriptor_size;
+    for (UINTN i = 0; i < buf_size; i++) {
+        EFI_MEMORY_DESCRIPTOR *o = (EFI_MEMORY_DESCRIPTOR *) (buf + i * discriptor_size);
+        Print(L"Type:%d PhysicalStart:%x VirtualStart:%x NumberOfPages:%d Attribute:%d\n", o->Type, o->PhysicalStart, o->VirtualStart, o->NumberOfPages, o->Attribute);
+    }
 
-    Print(L"buf_size %d\n", buf_size);
 
     Print(L"DONE.");
     for (;;)
