a52a169e3257fb9f93dd9623ca47f77c4b4e10b1
boxnos <boxnos@yahoo.com>
2021-06-16 01:05:54 +0900

    Add new, delete

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,6 +1,5 @@
+#include <cstddef>
 #include <cstdint>
-#include <algorithm>
-#include <cmath>
 #include <cstdlib>
 
 #include "frame_buffer_config.hpp"
@@ -10,33 +9,59 @@ struct color {
     uint8_t r, g, b;
 };
 
-int write_pixel(const frame_buffer_config &conf, int x, int y, const color &c) {
-    uint8_t *p {&conf.buf[4 * (conf.d * y + x)]};
-    switch (conf.f) {
-    case RGB:
-        p[0] = c.r;
-        p[1] = c.g;
-        p[2] = c.b;
-        break;
-    case BGR:
-        p[0] = c.b;
-        p[1] = c.g;
-        p[2] = c.r;
-        break;
-    default:
-        return 1;
-    }
-    return 0;
-}
+class pixel_writer {
+    private:
+        const frame_buffer_config conf;
+    public:
+        pixel_writer(const frame_buffer_config &c) : conf {c} {}
+        virtual ~pixel_writer() = default;
+        virtual void write(int x, int y, const color &c) = 0;
+    protected:
+        uint8_t* at(int x, int y) {
+            return &conf.buf[4 * (conf.d * y + x)];
+        }
+};
+
+class rgb_writer : public pixel_writer {
+    public:
+        using pixel_writer::pixel_writer;
+        virtual void write(int x, int y, const color &c) override {
+            auto p = at(x, y);
+            p[0] = c.r;
+            p[1] = c.g;
+            p[2] = c.b;
+        }
+};
+
+class bgr_writer : public pixel_writer {
+    public:
+        using pixel_writer::pixel_writer;
+        virtual void write(int x, int y, const color &c) override {
+            auto p = at(x, y);
+            p[0] = c.b;
+            p[1] = c.g;
+            p[2] = c.r;
+        }
+};
+
+void* operator new(size_t s, void *buf) { return buf; }
+void operator delete(void *o) {}
 
 extern "C" void kernel_main (const frame_buffer_config &conf) {
+    char writer_buf[sizeof(pixel_writer)];
+    pixel_writer *writer;
+    if (conf.f == RGB)
+        writer = new(writer_buf) rgb_writer(conf);
+    else
+        writer = new(writer_buf) bgr_writer(conf);
+
     for (int x {}; x < conf.h; ++x)
         for (int y {}; y < conf.v; ++y)
-            write_pixel(conf, x, y, {255, 255, 255});
+            writer->write(x, y, {255, 255, 255});
     for (int t {}; t < 10; t++)
         for (int x {}, ex = rand() % conf.h / 2, ey = rand() % conf.v / 2; x < ex; x++)
             for (int y {}; y < ey; y++)
-                write_pixel(conf, x + ex, y + ey, {(uint8_t)(ex % 256), (uint8_t)(ey % 256), 0});
+                writer->write(x + ex, y + ey, {(uint8_t)(ex % 256), (uint8_t)(ey % 256), 0});
     for (;;)
         __asm__("hlt");
 }
