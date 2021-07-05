b1f28297e2272c1d918fac10d0ed0a61619ea9cd
boxnos <boxnos@yahoo.com>
2021-07-06 02:37:11 +0900

    Add printk

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -7,8 +7,25 @@
 #include "console.hpp"
 using namespace std;
 
+char konsole_buf[sizeof(console)];
+console *konsole;
+
 void* operator new([[maybe_unused]] size_t s, void *buf) { return buf; }
 void operator delete([[maybe_unused]] void * o) noexcept {}
+
+int printk(const char* s, ...) {
+    int r;
+    va_list ap;
+    static char b[1024];
+
+    va_start(ap, s);
+    r = vsprintf(b, s, ap);
+    va_end(ap);
+
+    konsole->put_string(b);
+
+    return r;
+}
 
 extern "C" void kernel_main (const frame_buffer_config &conf) {
     char writer_buf[sizeof(pixel_writer)];
@@ -22,18 +39,16 @@ extern "C" void kernel_main (const frame_buffer_config &conf) {
             for (int y: range(ey))
                 writer->write(x + ex, y + ey, {(uint8_t)(ex % 256), (uint8_t)(ey % 256), 0});
 
+    konsole = new(konsole_buf) console(*writer, {0xFF, 0xFF, 0xFF}, {0x33, 0x33, 0x33});
+
     char buf[1024];
+    write_string(*writer, 200, 300, "Hello, boxnos-m World !?", {0xFF, 0xFF, 0xFF});
+    sprintf(buf, "123 * 456 = %d", 123 * 456);
+    write_string(*writer, 200, 313, buf, {0xFF, 0xFF, 0xFF});
 
-//    write_string(*writer, 200, 300, "Hello, boxnos-m World !?", {0xFF, 0xFF, 0xFF});
-//    sprintf(buf, "123 * 456 = %d", 123 * 456);
-//    write_string(*writer, 200, 313, buf, {0xFF, 0xFF, 0xFF});
-
-    console c(*writer, {0xFF, 0xFF, 0xFF}, {0x33, 0x33, 0x33});
-    for (int i: range(1, 20)) {
-        sprintf(buf, "long long long long long line : %d\n", i);
-        c.put_string(buf);
-    }
-    c.put_string("Wellcome to the boxnos world!!\n");
+    for (int i: range(1, 20))
+        printk("long long long long long line : %d\n", i);
+    printk("Wellcome to the boxnos world!!\n");
 
     for (;;)
         __asm__("hlt");
